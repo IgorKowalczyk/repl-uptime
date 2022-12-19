4096d4d05f8046fcfe22b24e5091e69ca37e35c1
Igor Kowalczyk <majonezexe@protonmail.com>
2022-12-19 21:45:23 +0100

    Add eslint config

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { createServer } = require("node:http");
 
 function config(options = {}) {
  let port = options.port || 8080;
- let custom_url = options.path ? options.path.toString() : "/";
- let custom_response = options.message ? options.message.toString() : "200 OK!";
+ let customURL = options.path ? options.path.toString() : "/";
+ let customResponse = options.message ? options.message.toString() : "200 OK!";
  const request = async (req, res) => {
   if (options.debug) console.log(`::debug:: [repl-uptime] => ${req.method.toLowerCase()} ${req.url}`);
-  if (req.url === custom_url) {
+  if (req.url === customURL) {
    res.writeHead(200);
-   return res.end(custom_response);
+   return res.end(customResponse);
   }
  };
  const server = createServer(request);
